a7278d97a5652f0d4d5d835bd29faa532be774fc
Pierre Poupin <pierrep@bam.tech>
2020-11-07 20:14:24 +0100

    feature: add Barrel

--- Tank/Tank.gd
+++ Tank/Tank.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
+const type = "TANK"
+
 # TODO get this value from a config somewhere
-const GRID_SIZE = 100
+const GRID_SIZE = 128
 
 var grid
 
--- TileMap.gd
+++ TileMap.gd
@@ -1,41 +1,57 @@
 extends TileMap
 class_name Grid
 
-const HEIGHT = 4
-const WIDTH = 4
+const HEIGHT = 5
+const WIDTH = 8
 
-var tank_positions = []
+var object_positions = []
+
 
 var Tank = preload("res://Tank/Tank.tscn")
 
 func _ready():
-	make_grid()
+	object_positions = make_grid()
+	register_children_objects()	
 	place_tank(1, 1)
+	print(object_positions)
 	
-	print(tank_positions)
+func register_children_objects():
+	var children = self.get_children()
+	print(children)
+	for child in children: 
+		var child_map_position = self.world_to_map(child.position)
+		register_object(child_map_position.x, child_map_position.y, child.type)
 
 func make_grid():
+	var array = []
 	for y in HEIGHT:
 		var line = []
 		for x in WIDTH:
 			line.append(null)
-		tank_positions.append(line)
+		array.append(line)
+	
+	return array
+
+
 
 func place_tank(x,y):
 	var new_tank = Tank.instance()
 	
 	var world_position = self.map_to_world(Vector2(x, y))
 	new_tank.position = world_position
-	tank_positions[y][x] = "TANK"
+
+	register_object(x,y,"TANK")
 	self.add_child(new_tank)
 	
+func register_object(x,y,object):
+	object_positions[y][x] = object
 
 func move_if_possible(tank, direction: Vector2):
 	var pos = world_to_map(tank.position)
-	var next_pos = Vector2(pos.y + direction.y, pos.x + direction.x)
+	var next_pos = Vector2(pos.x + direction.x, pos.y + direction.y)
 	if can_move(pos, next_pos):
-		tank_positions[pos.y][pos.x] = null
-		tank_positions[pos.y + direction.y][pos.x + direction.x] = "TANK"
+		object_positions[pos.y][pos.x] = null
+		object_positions[pos.y + direction.y][pos.x + direction.x] = "TANK"
 		
 		return true
 	return false
@@ -43,6 +59,10 @@ func move_if_possible(tank, direction: Vector2):
 func can_move(initial_position: Vector2, next_position: Vector2):
 	if next_position.x < 0 || next_position.y <0:
 		return false
-	if next_position.x >= tank_positions[0].size() || next_position.y >= tank_positions.size():
+	if next_position.x >= object_positions[0].size() || next_position.y >= object_positions.size():
 		return false
+		
+	if object_positions[next_position.y][next_position.x] != null:
+		return false
+		
 	return true
