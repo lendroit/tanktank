e0afd53cdbd0a4e71518f8a178011e87b9d87e13
Pierre Poupin <pierrep@bam.tech>
2020-11-10 19:58:37 +0100

    remove animation player

--- Player.gd
+++ Player.gd
@@ -16,8 +16,6 @@ var action_list
 func _ready():
 	position = position.snapped(Vector2.ONE * tile_size)
 	position += Vector2.ONE * tile_size/2
-	# Adjust animation speed to match movement speed
-	$AnimationPlayer.playback_speed = speed
 
 func start_turn(new_action_list):
 	action_list = new_action_list
@@ -36,7 +34,6 @@ func move(dir):
 	ray.cast_to = inputs[dir] * tile_size
 	ray.force_raycast_update()
 	if !ray.is_colliding():
-		$AnimationPlayer.play(dir)
 		move_tween(inputs[dir])
 		
 func move_tween(dir):
