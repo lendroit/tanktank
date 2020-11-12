8fbd3c0d39782f13496175c9f8971fa01ee8e62c
Pierre Poupin <pierrep@bam.tech>
2020-11-13 00:15:47 +0100

    feature: add P1/P2 ui difference

--- UI/ActionIndicator.gd
+++ UI/ActionIndicator.gd
@@ -1,14 +1,18 @@
 extends TextureRect
 
-var active_texture = preload("res://UI/blue_boxTick.png")
+export(int, 1, 2) var player_id = 1
+
+var active_texture_for_player = {
+	1: preload("res://UI/green_boxTick.png"),
+	2: preload("res://UI/blue_boxTick.png")
+}
 var inactive_texture = preload("res://UI/grey_circle.png")
 
 var is_active = false
 
-# TODO needs a P1/P2 color mode
 
 func enable():
-	self.texture = active_texture
+	self.texture = active_texture_for_player[player_id]
 	is_active = true
 	
 func disable():
--- UI/ActionIndicatorContainer.gd
+++ UI/ActionIndicatorContainer.gd
@@ -1,5 +1,11 @@
 extends HBoxContainer
 
+export(int, 1, 2) var player_id = 1
+
+func _ready():
+	for action_indicator in get_children():
+		action_indicator.player_id = player_id
+
 func add_action():
 	for action_indicator in get_children():
 		if !action_indicator.is_active:
